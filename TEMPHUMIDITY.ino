fc1a6937c64316a9e3dc268d0cf55055770ee5ff
leonardoman9 <46076388+leonardoman9@users.noreply.github.com>
2022-01-28 20:24:52 +0100

    Sensore temperatura e umidità con display

---
+++ TEMPHUMIDITY.ino
@@ -0,0 +1,33 @@
+#include <dht.h>
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+dht DHT;
+#define DHT11_PIN 7
+
+void setup() {
+ Serial.begin(9600);
+ lcd.begin(16, 2);
+ lcd.print("Temp: Umidita':");
+}
+
+void loop() {
+   int chk = DHT.read11(DHT11_PIN);
+  Serial.print("Temperature = ");
+  Serial.println(DHT.temperature);
+  Serial.print("Humidity = ");
+  Serial.println(DHT.humidity);
+   delay(50);
+  lcd.setCursor(0, 1);
+  int  t = (int) DHT.temperature;
+  int h = (int) DHT.humidity;
+  Serial.println(t);
+  Serial.println(h);
+  lcd.print(t);
+  lcd.print((char)223);
+  lcd.print("C");
+  lcd.setCursor(7,1);
+  lcd.print(h);  
+  lcd.print("%");
+}

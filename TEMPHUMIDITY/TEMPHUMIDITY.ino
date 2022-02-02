f43501913c2f25dea3bc86102721082f7fb513c0
leonardoman9 <46076388+leonardoman9@users.noreply.github.com>
2022-02-02 21:45:57 +0100

    modificato criterio di accensione ventola
    la ventola si accende non più a seconda della temperatura, ma per 10 minuti ogni 2 ore.

--- TEMPHUMIDITY/TEMPHUMIDITY.ino
+++ TEMPHUMIDITY/TEMPHUMIDITY.ino
@@ -1,11 +1,18 @@
+#include <TimeLib.h>
+
 #include <dht.h>
 #include <LiquidCrystal.h>
 
+ int t, h;
+ 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 dht DHT;
 #define DHT11_PIN 7
-int t;
-int h;
+unsigned long previousMillis = 0;    
+const long interval = 1000*1*60*10; //10 minuti
+const long intervalLong = 1000*1*60*60*2;   //2 ore
+
+  int state=LOW;
 void setup() {
  Serial.begin(9600);
  lcd.begin(16, 2);
@@ -14,25 +21,50 @@ void setup() {
 }
 
 void loop() {
+  time_t t = now();
    int chk = DHT.read11(DHT11_PIN);
    delay(50);
-      t = (int) DHT.temperature;
-      h = (int) DHT.humidity;
   printTH();
-
-  if(t>25) {
-     digitalWrite(13, HIGH); // sets the digital pin 13 on
-  } else {
-    digitalWrite(13, LOW);  // sets the digital pin 13 off
-    }
+  fanControl();
   
 }
 
+void fanControl(){
+   unsigned long currentMillis = millis();
+   Serial.println("Stato:");
+  Serial.println(state);
+  Serial.println("Current millis:");
+  Serial.println(currentMillis);
+  Serial.println("Previous millis:");
+  Serial.println(previousMillis);
+ Serial.println("Intervallo:");
+ Serial.println(currentMillis - previousMillis);
+ Serial.println();
+ if (state==LOW){   //se la ventola è spenta
+  if (currentMillis - previousMillis >= intervalLong) {     //si accende dopo 20 secondi
+      previousMillis = currentMillis;
+    digitalWrite(13, HIGH);
+      state = HIGH;
+  }
+ }
+ else if (state==HIGH) {    //se la ventola è accesa
+  if (currentMillis - previousMillis >= interval) {   //si spegne dopo 10 secondi
+      previousMillis = currentMillis;
+    digitalWrite(13, LOW);
+      state = LOW;
+  }
+ }
+}
+
+
+
 void printTH(){
-  Serial.print("Temperature = ");
-  Serial.println(DHT.temperature);
-  Serial.print("Humidity = ");
-  Serial.println(DHT.humidity);
+   t = (int) DHT.temperature;
+   h = (int) DHT.humidity;
+//  Serial.print("Temperature = ");
+//  Serial.println(DHT.temperature);
+//  Serial.print("Humidity = ");
+//  Serial.println(DHT.humidity);
   lcd.setCursor(1, 1);
   lcd.print(t);
   lcd.print((char)223);

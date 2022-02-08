d1b87a5d1afab0a5bb48eb427739264d32ac0423
leonardoman9 <46076388+leonardoman9@users.noreply.github.com>
2022-02-09 00:14:03 +0100

    minor changes

--- TEMPHUMIDITY/TEMPHUMIDITY.ino
+++ TEMPHUMIDITY/TEMPHUMIDITY.ino
@@ -4,23 +4,28 @@
 #include <LiquidCrystal.h>
 
  int t, h;
+ long minuti(int a);
  
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 dht DHT;
 #define DHT11_PIN 7
 unsigned long previousMillis = 0;    
-const long interval = 1000*1*60*10; //10 minuti
-const long intervalLong = 1000*1*60*60*2;   //2 ore
+unsigned long interval = minuti(5); 
+unsigned long intervalLong = minuti(10); 
 
-  int state=LOW;
+int state=LOW;
 void setup() {
  Serial.begin(9600);
+ Serial.println("START");
  lcd.begin(16, 2);
  lcd.print("Temp:  Umidita':");
  pinMode(13, OUTPUT);    // sets the digital pin 13 as output
 }
 
 void loop() {
+  Serial.print("Cambio di stato della ventola fra ");
+  Serial.print(intervalLong/1000 -(millis()/1000 - previousMillis/1000));
+  Serial.println(" secondi");
   time_t t = now();
    int chk = DHT.read11(DHT11_PIN);
    delay(50);
@@ -57,4 +62,8 @@ void printTH(){
   lcd.setCursor(13,1);
   lcd.print(h);  
   lcd.print("%");
+}
+
+long minuti(int a){
+  return a*60000;
 }

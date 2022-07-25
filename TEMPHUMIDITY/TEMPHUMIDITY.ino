b3b828076d2d24ca66ccb804d2d170ddba56fb5a
leonardoman9 <46076388+leonardoman9@users.noreply.github.com>
2022-07-25 18:33:31 +0200

    aggiunto moist sensor, sistemato lcd

--- TEMPHUMIDITY/TEMPHUMIDITY.ino
+++ TEMPHUMIDITY/TEMPHUMIDITY.ino
@@ -6,17 +6,30 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 dht DHT;
 #define PHOTO_IN A0
 #define DHT11_PIN 7
+#define MOIST A1
+#define REFRESH_RATE 20
+
+int frame=0;
 unsigned long previousMillis = 0;
 unsigned long interval = minuti(1);
 unsigned long intervalLong = minuti(1);
 int lightMax = 600;
 int fanState = LOW;
+int temperature;
+int humidity;
+int moist;
 
 void setup() {
   Serial.begin(9600);
   Serial.println("START");
   lcd.begin(16, 2);
-  lcd.print("Temp:  Umidita':");
+  lcd.setCursor(0, 0);
+  lcd.print("t:");
+  lcd.setCursor(5,0);
+  lcd.print("m:");
+  lcd.setCursor(13,0);
+  lcd.print("h:");
+
   pinMode(13, OUTPUT);
   pinMode(10, OUTPUT);
 }
@@ -25,17 +38,30 @@ void loop() {
   printTH();
   lightControl();
   fanControl();
+  moistControl();
 }
 
 void lightControl() {
   int lightVal = analogRead(PHOTO_IN);
-  Serial.println(lightVal);
   if (lightVal < lightMax) {
     digitalWrite(10, HIGH);
   }
   else {
     digitalWrite(10, LOW);
   }
+}
+
+void moistControl(){
+  int moistVal = analogRead(MOIST);
+  Serial.println(moistVal);
+  if(moistVal!= moist){
+    moist = moistVal;
+
+    lcd.setCursor(5,1);
+    lcd.print("    ");
+    lcd.setCursor(5,1);
+    lcd.print(moistVal);
+   }
 }
 
 void fanControl() {
@@ -63,13 +89,37 @@ void printTH() {
   int chk = DHT.read11(DHT11_PIN);
   int t = (int) DHT.temperature;
   int h = (int) DHT.humidity;
-  lcd.setCursor(1, 1);
-  lcd.print(t);
-  lcd.print((char)223);
-  lcd.print("C");
-  lcd.setCursor(13, 1);
-  lcd.print(h);
-  lcd.print("%");
+  if (frame==REFRESH_RATE) {
+    lcd.clear();
+    lcd.begin(16, 2);
+     lcd.setCursor(0, 0);
+    lcd.print("t:");
+    lcd.setCursor(5,0);
+    lcd.print("m:");
+    lcd.setCursor(13,0);
+    lcd.print("h:");
+
+    frame=0;
+    Serial.println("LCD pulito");
+    }
+  if (t!=temperature || h != humidity){
+          frame++;
+
+    temperature = t;
+    humidity = h;
+    lcd.setCursor(0, 1);
+    lcd.print("     ");
+    lcd.setCursor(0, 1);
+    lcd.print(t);
+    lcd.print((char)223);
+    lcd.print("C");
+    lcd.setCursor(13, 1);
+    lcd.print("    ");
+    lcd.setCursor(13, 1);
+    lcd.print(h);
+    lcd.print("%");
+  }
+
 }
 
 long minuti(int a) {
